6c151ca1f62af5369753c02543088b87bd4a24e0
Clifford onyeka <chukwugolumog14541@gmail.com>
2023-07-18 05:54:28 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readme for C-functions and nested loops
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include "unistd.h"
+/**
+ * main: Entry point
+ * Return: 0
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}

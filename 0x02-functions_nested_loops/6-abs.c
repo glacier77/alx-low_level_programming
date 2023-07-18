ecda06cee6258611dfe5e33fa042c6e199f967eb
Clifford Onyeka <chukwugolumog14541@gmail.com>
2023-07-18 12:13:03 -0600

    jack_bauer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an int
+ *
+ * @i: input the number as integer
+ *
+ * Return: absolute value
+ */
+int _abs(int)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a num
+ *
+ * @n: input num as an int
+ *
+ * Return: last digit.
+ */
+int print_last_digit(int)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}#include "main.h"
+/**
+ * print_last_digit - Prints the last digit of a num
+ *
+ * @n: input num as an int
+ *
+ * Return: last digit.
+ */
+int print_last_digit(int)
+{
+	int l;
+
+	l = n % 10;
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (l);
+	}
+}
